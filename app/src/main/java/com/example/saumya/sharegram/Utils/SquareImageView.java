c1742f72f51eaa4ba4d43c0e5f9d0659e1e5208a
karan17235 <1karanbansal@gmail.com>
2018-10-21 21:40:59 +1100

    Removed Unused Comments

--- app/src/main/java/com/example/saumya/sharegram/Utils/SquareImageView.java
+++ app/src/main/java/com/example/saumya/sharegram/Utils/SquareImageView.java
@@ -4,10 +4,6 @@ import android.content.Context;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
-/**
- * Created by User on 6/4/2017.
- */
-
 public class SquareImageView extends AppCompatImageView {
 
     public SquareImageView(Context context) {
